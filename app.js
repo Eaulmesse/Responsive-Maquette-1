9b037499af4f2dd00654cc165d701793fcdda440
Eaulmesse <theo.guerin01120@gmail.com>
2022-05-27 16:08:24 +0200

    feat: adding 1/2 burger js

---
+++ app.js
@@ -0,0 +1,6 @@
+let burgerButton = document.querySelector('.burger-button');
+let navbarResponsive = document.querySelector('.navbar-responsive');
+
+burgerButton.addEventListener("click", function(){
+    navbarResponsive.style.display = "block";
+}); 
