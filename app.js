363b52a67d57807ef0554ea04cffbe67f91fbc09
Eaulmesse <theo.guerin01120@gmail.com>
2022-05-30 14:35:56 +0200

    feat: finishing the code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let burgerButton = document.querySelector('.burger-button');
-let navbarResponsive = document.querySelector('.navbar-responsive');
+let navbarResponsive = document.getElementById('navBarJs');
 
 burgerButton.addEventListener("click", function(){
-    navbarResponsive.style.display = "block";
+    
+    navbarResponsive.classList.toggle('display-none');
 }); 
